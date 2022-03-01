ae0783a5806b58c260a4233ccc29f26da2543b57
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-03-01 22:20:06 +0500

    [feat] Add preload script

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
-	let win = new BrowserWindow();
+	let win = new BrowserWindow({
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+		},
+	});
 	win.maximize();
 	win.loadFile('./index.html');
 };
